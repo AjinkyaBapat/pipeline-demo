f164ffbe3711239a99680269deebd1c015887101
Ajinkya <ajinkyabapat12@gmail.com>
2020-11-03 11:20:54 +0530

    Update test-all.sh

--- jenkins/test-all.sh
+++ jenkins/test-all.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-chmod -R 755 $DIR
-$DIR/test-backend.sh
-$DIR/test-frontend.sh
-$DIR/test-static.sh
-$DIR/test-performance.sh
+bash $DIR/test-backend.sh
+bash $DIR/test-frontend.sh
+bash $DIR/test-static.sh
+bash $DIR/test-performance.sh
