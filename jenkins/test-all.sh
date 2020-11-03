8b0f99eb6c1768c0026d372e363634c6066b0590
AjinkyaBapat <ajinkyabapat12@gmail.com>
2020-11-03 11:04:57 +0530

    Updated permissions

--- jenkins/test-all.sh
+++ jenkins/test-all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
 $DIR/test-backend.sh
 $DIR/test-frontend.sh
 $DIR/test-static.sh
